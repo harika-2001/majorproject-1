e20a6eca4a4cc26c6817964e9be4f32f13b1d2e5
harika-2001 <65670592+harika-2001@users.noreply.github.com>
2021-05-17 17:55:34 +0530

    Add files via upload

---
+++ Questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "A Bird Which Can Fly backWards is?",
+    answer: "Humming Bird",
+    options: [
+      "WoodPicker",
+      "Sparrow",
+      "Humming Bird",
+      "Parrot"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Tallest Skyscraper in The World is?",
+    answer: "Burj Khalifa",
+    options: [
+      "Kingdom tower",
+      "Burj Khalifa",
+      "White House",
+      "Leaning Tower of Pisa"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Fastest train in The World is?",
+    answer: "Shanghai Maglev",
+    options: [
+      "Shanghai Maglev",
+      "Fuxing Hao CR400AF/BF",
+      "Harmony CRH 380A",
+      "Shinkansen E5, H5 Series"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Number of elements in the periodic table are?",
+    answer: "118",
+    options: [
+      "120",
+      "117",
+      "115",
+      "118"
+    ]
+  },
+];
